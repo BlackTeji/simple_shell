038aff8f06be417a346dcd51f642c6c77fc17dd6
Martinez <write2martinez@gmail.com>
2023-12-13 12:18:59 -0800

    Memory Related Funcs

---
+++ allocation_and_free.c
@@ -0,0 +1,64 @@
+#include "shell.h"
+
+/**
+ * _memset - Fills memory with a constant byte.
+ * @s: Pointer to the memory area.
+ * @b: The byte to fill *s with.
+ * @n: The number of bytes to be filled.
+ * Return: A pointer to the memory area s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
+ * ffree - Frees a string of strings.
+ * @pp: String of strings.
+ */
+
+void ffree(char **pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - Reallocates a block of memory.
+ * @ptr: Pointer to the previous malloc'ated block.
+ * @old_size: Byte size of the previous block.
+ * @new_size: Byte size of the new block.
+ * Return: Pointer to the old block or a new block.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
+}
